5c43533cd8ae31429a30d6d1204359c5ebb583a3
denballakh <denis.ballakh@gmail.com>
2024-02-18 01:34:52 +0300

    pull shape into its own object, update `__iter__`, implement `__r*__` methods

--- simd.py
+++ simd.py
@@ -14,72 +14,90 @@ else:
 
 @cache
 def mask(n: int, /) -> int:
-    """
+    """```
     11...11
     ^^^^^^^ - n bits
-    """
+    ```"""
     return ~(~0 << n)
 
 
 @cache
 def mask_array(n: int, bi: int, /) -> int:
-    """
+    """```
     00...001 00...001 ... 00...001 00...001
            ^        ^            ^        ^ - n items
     ^^^^^^^^ ^^^^^^^^     ^^^^^^^^ ^^^^^^^^ - bi bits each
-    """
-    # return int(f'{1:0{bi}b}' * n, 2)
+    ```"""
     return ~(~0 << n * bi) // ~(~0 << bi)
 
 
-@cache
-def _mask_array_val(n: int, bi: int, /) -> int:
+@dataclasses.dataclass(frozen=True)
+class S:
     """
-    0000 0101  0000 0100  0000 0011  0000 0010  0000 0001
-            ^          ^          ^          ^          ^
+    shape of an array
     """
-    # works like array of 1
-    return mask_array(n, bi)
 
+    len: int  # number of items
+    bv: int  # number of value bits in each item
+    bp: int  # number of padding bits in each item
 
-@cache
-def _mask_val(n: int, bp: int, bv: int, /) -> int:
-    """
-    0000 0101  0000 0100  0000 0011  0000 0010  0000 0001
-         ^^^^       ^^^^       ^^^^       ^^^^       ^^^^
-    """
-    # works like array of MAXVAL
-    return _mask_array_val(n, bp + bv) * mask(bv)
+    @property
+    def bi(self, /) -> int:
+        return self.bp + self.bv
 
+    @property
+    @cache
+    def mask(self, /) -> int:
+        """```
+        0000 0101  0000 0100  0000 0011  0000 0010  0000 0001
+        ^^^^ ^^^^  ^^^^ ^^^^  ^^^^ ^^^^  ^^^^ ^^^^  ^^^^ ^^^^
+        ```"""
+        return mask(self.bi * self.len)
 
-@cache
-def _mask_array_pad(n: int, bp: int, bv: int, /) -> int:
-    """
-    0000 0101  0000 0100  0000 0011  0000 0010  0000 0001
-       ^          ^          ^          ^          ^
-    """
-    # works like array of MAXVAL+1
-    return mask_array(n, bp + bv) << bv
+    @property
+    @cache
+    def mask_array_val(self, /) -> int:
+        """```
+        0000 0001  0000 0001  0000 0001  0000 0001  0000 0001
+                ^          ^          ^          ^          ^
+        ```"""
+        # works like array of 1
+        return mask_array(self.len, self.bi)
 
+    @property
+    @cache
+    def mask_val(self, /) -> int:
+        """```
+        0000 1111  0000 1111  0000 1111  0000 1111  0000 1111
+             ^^^^       ^^^^       ^^^^       ^^^^       ^^^^
+        ```"""
+        # works like array of MAXVAL
+        return self.mask_array_val * mask(self.bv)
 
-@cache
-def _mask_pad(n: int, bp: int, bv: int, /) -> int:
-    """
-    0000 0101  0000 0100  0000 0011  0000 0010  0000 0001
-    ^^^^       ^^^^       ^^^^       ^^^^       ^^^^
-    """
-    return _mask_array_pad(n, bp, bv) * mask(bp)
+    @property
+    @cache
+    def mask_array_pad(self, /) -> int:
+        """```
+        0001 0000  0001 0000  0001 0000  0001 0000  0001 0000
+           ^          ^          ^          ^          ^
+        ```"""
+        # works like array of MAXVAL+1
+        return mask_array(self.len, self.bi) << self.bv
 
-
-@dataclasses.dataclass
-class Shape:
-    pass
+    @property
+    @cache
+    def mask_pad(self, /) -> int:
+        """```
+        1111 0000  1111 0000  1111 0000  1111 0000  1111 0000
+        ^^^^       ^^^^       ^^^^       ^^^^       ^^^^
+        ```"""
+        return self.mask_array_pad * mask(self.bp)
 
 
 class A:
     """
-    A(0x0504030201, *(4, 4, 5)):
-
+    ```
+    A(0x_05_04_03_02_01, S(4, 4, 5)):
        0    5     0    4     0    3     0    2     0    1
     0000 0101  0000 0100  0000 0011  0000 0010  0000 0001
     ^^^^       ^^^^       ^^^^       ^^^^       ^^^^      - padding
@@ -87,265 +105,234 @@ class A:
     ^^^^^^^^^  ^^^^^^^^^  ^^^^^^^^^  ^^^^^^^^^  ^^^^^^^^^ - items
     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ - data
             4          3          2          1          0 - items are indexed from least significant to most significant
+    ```
 
     assumptions:
-      padding bits are always filled with zeros
-      all attrs makes sense
-      .data is nonnegative
-      if operation requires padding, then it is performed on arrays that do have enough padding, otherwise behaviour is unspecified
+      - padding bits are always filled with zeros
+      - all attrs makes sense
+      - .data is nonnegative
+      - if operation requires padding, then it is performed on arrays that do have   enough padding, otherwise behaviour is unspecified
 
     """
 
+    __match_args__ = __slots__ = (
+        'data',
+        's',
+    )
     data: int
-    bits_val: int
-    bits_pad: int
-    length: int
+    s: S
 
-    def __init__(self, data: int, bv: int, bp: int, length: int, /) -> None:
+    def __init__(self, data: int, s: S, /) -> None:
+        assert data >= 0, f'negative data: {data}'
         self.data = data
-        self.bits_val = bv
-        self.bits_pad = bp
-        self.length = length
+        self.s = s
+        # clear padding bits if they are set for some reason:
+        self.data &= self.s.mask_val
+
+    @classmethod
+    def from_const(cls, val: int, s: S, /) -> t.Self:
+        return cls(s.mask_array_val * val, s)
 
     def __eq__(self, other: object, /) -> bool:
         if other.__class__ is not A:
             return NotImplemented
         assert isinstance(other, A), other
-        return self._shape == other._shape and self.data == other.data
+        return self.s == other.s and self.data == other.data
 
     def __ne__(self, other: object, /) -> bool:
         if other.__class__ is not A:
             return NotImplemented
         assert isinstance(other, A), other
-        return self._shape != other._shape or self.data != other.data
+        return self.s != other.s or self.data != other.data
 
-    def _from_const(self, n: int, /) -> A:
-        return A(self._mask_array_val * n, *self._shape)
-
-    @property
-    def _shape(self, /) -> tuple[int, int, int]:
-        return self.bits_val, self.bits_pad, self.length
-
-    @property
-    def _bits_item(self, /) -> int:
-        return self.bits_pad + self.bits_val
-
-    @property
-    def mask(self, /) -> int:
-        """
-        0000 0101  0000 0100  0000 0011  0000 0010  0000 0001
-        ^^^^ ^^^^  ^^^^ ^^^^  ^^^^ ^^^^  ^^^^ ^^^^  ^^^^ ^^^^
-        """
-        return mask(self._bits_item * self.length)
-
-    @property
-    def _mask_array_val(self, /) -> int:
-        return _mask_array_val(self.length, self._bits_item)
-
-    @property
-    def _mask_val(self, /) -> int:
-        return _mask_val(self.length, self.bits_pad, self.bits_val)
-
-    @property
-    def _mask_array_pad(self, /) -> int:
-        return _mask_array_pad(self.length, self.bits_pad, self.bits_val)
-
-    @property
-    def _mask_pad(self, /) -> int:
-        return _mask_pad(self.length, self.bits_pad, self.bits_val)
+    def _get_binop_operand(self, other: A | int) -> A:
+        if isinstance(other, int):
+            return self.from_const(other, self.s)
+        if self.s != other.s:
+            raise ValueError(f'incompatible shapes: {self.s} and {other.s}')
+        return other
 
     def _get_item(self, i: int, /) -> int:
-        return self.data >> (i * self._bits_item) & mask(self._bits_item)
+        return self.data >> (i * self.s.bi) & mask(self.s.bi)
 
     def _get_padval(self, i: int, /) -> tuple[int, int]:
         item = self._get_item(i)
-        pad = item >> self.bits_val
-        val = item & mask(self.bits_val)
+        pad = item >> self.s.bv
+        val = item & mask(self.s.bv)
         return pad, val
 
-    def _get_pad(self, i: int, /) -> int:
-        return self._get_padval(i)[0]
-
-    def _get_val(self, i: int, /) -> int:
-        return self._get_padval(i)[1]
-
     def __iter__(self) -> t.Iterator[int]:
-        # TODO: do it in O(n), not O(n^2)
-        for i in range(self.length):
-            yield self._get_val(i)
+        nbits = self.s.bi * self.s.len
+        bits = bin(self.data)[2:].zfill(nbits)[::-1]
+        for i in range(self.s.len):
+            x = bits[i * self.s.bi : (i + 1) * self.s.bi]
+            yield int(x[: self.s.bv][::-1], 2)
 
     def __str__(self, /) -> str:
         res = []
-        for i in range(self.length):
+        for i in range(self.s.len):
             pad, val = self._get_padval(i)
-            res += [f"{pad:0{self.bits_pad}b}_{val:0{self.bits_val}b}"]
+            res += [f"{pad:0{self.s.bp}b}_{val:0{self.s.bv}b}"]
         return f"[{", ".join(res)}]"
 
     def __repr__(self, /) -> str:
         bits_per_hex_digit = 4
-        hex_digits = self._bits_item * self.length / bits_per_hex_digit
+        hex_digits = self.s.bi * self.s.len / bits_per_hex_digit
         hex_digits = hex_digits.__ceil__()
-        return f"{self.__class__.__qualname__}({self.data:#0{hex_digits+2}x}, *{self._shape})"
+        return f"{self.__class__.__qualname__}({self.data:#0{hex_digits+2}x}, {self.s})"
 
-    def __add__(self, other: A | int, /) -> A:
-        if isinstance(other, int):
-            other = self._from_const(other)
-        if self._shape != other._shape:
-            raise ValueError
+    def __add__(self, other: A | int, /) -> t.Self:
+        other = self._get_binop_operand(other)
         n1 = self.data
         n2 = other.data
         # perform addition:
         n = n1 + n2
         # fill padding with zeros:
-        n &= self._mask_val
-        return A(n, *self._shape)
+        n &= self.s.mask_val
+        return self.__class__(n, self.s)
 
-    def __sub__(self, other: A | int, /) -> A:
-        if isinstance(other, int):
-            other = self._from_const(other)
-        if self._shape != other._shape:
-            raise ValueError
+    def __radd__(self, other: int, /) -> t.Self:
+        return self + other
+
+    def __sub__(self, other: A | int, /) -> t.Self:
+        other = self._get_binop_operand(other)
         n1 = self.data
         n2 = other.data
         # store 1 in each padding block:
-        n1 |= self._mask_array_pad
+        n1 |= self.s.mask_array_pad
         # perform subtraction:
         n = n1 - n2
         # fill padding with zeros:
-        n &= self._mask_val
-        return A(n, *self._shape)
+        n &= self.s.mask_val
+        return self.__class__(n, self.s)
 
-    def __mul__(self, other: A | int, /) -> A:
+    def __rsub__(self, other: int, /) -> t.Self:
+        return self.from_const(other, self.s) - self
+
+    def __mul__(self, other: A | int, /) -> t.Self:
         if isinstance(other, int):
             # multiplication by constant
             n = self.data
             n *= other
-            n &= self._mask_val
-            return A(n, *self._shape)
+            n &= self.s.mask_val
+            return self.__class__(n, self.s)
 
-        if self._shape != other._shape:
+        if self.s != other.s:
             raise ValueError
 
         n1 = self.data
         n2 = other.data
         n = 0
-        for i in range(self.bits_val):
+        for i in range(self.s.bv):
             b = n2
             b >>= i
-            b &= self._mask_array_val
-            b *= mask(self.bits_val)
+            b &= self.s.mask_array_val
+            b *= mask(self.s.bv)
             b &= n1
             b <<= i
             n += b
 
-        n &= self._mask_val
-        return A(n, *self._shape)
+        n &= self.s.mask_val
+        return self.__class__(n, self.s)
 
-    def __and__(self, other: A | int, /) -> A:
-        if isinstance(other, int):
-            other = self._from_const(other)
-        if self._shape != other._shape:
-            raise ValueError
+    def __rmul__(self, other: int, /) -> t.Self:
+        return self * other
+
+    def __and__(self, other: A | int, /) -> t.Self:
+        other = self._get_binop_operand(other)
         n1 = self.data
         n2 = other.data
         n = n1 & n2
-        return A(n, *self._shape)
+        return self.__class__(n, self.s)
 
-    def __or__(self, other: A | int, /) -> A:
-        if isinstance(other, int):
-            other = self._from_const(other)
-        if self._shape != other._shape:
-            raise ValueError
+    def __rand__(self, other: int, /) -> t.Self:
+        return self & other
+
+    def __or__(self, other: A | int, /) -> t.Self:
+        other = self._get_binop_operand(other)
         n1 = self.data
         n2 = other.data
         n = n1 | n2
-        return A(n, *self._shape)
+        return self.__class__(n, self.s)
 
-    def __xor__(self, other: A | int, /) -> A:
-        if isinstance(other, int):
-            other = self._from_const(other)
-        if self._shape != other._shape:
-            raise ValueError
+    def __ror__(self, other: int, /) -> t.Self:
+        return self | other
+
+    def __xor__(self, other: A | int, /) -> t.Self:
+        other = self._get_binop_operand(other)
         n1 = self.data
         n2 = other.data
         n = n1 ^ n2
-        return A(n, *self._shape)
+        return self.__class__(n, self.s)
 
-    def __rshift__(self, b: int, /) -> A:
+    def __rxor__(self, other: int, /) -> t.Self:
+        return self ^ other
+
+    def __rshift__(self, b: int, /) -> t.Self:
         n = self.data
-        n >>= b
-        n &= self._mask_val
-        return A(n, *self._shape)
+        n >>= b  # padding will be cleared later
+        return self.__class__(n, self.s)
 
-    def __lshift__(self, b: int, /) -> A:
+    def __lshift__(self, b: int, /) -> t.Self:
         n = self.data
-        n <<= b
-        n &= self._mask_val
-        return A(n, *self._shape)
+        n <<= b  # padding will be cleared later
+        return self.__class__(n, self.s)
 
-    def __invert__(self, /) -> A:
+    def __invert__(self, /) -> t.Self:
         n = self.data
-        n ^= self._mask_val
-        return A(n, *self._shape)
+        n ^= self.s.mask_val
+        return self.__class__(n, self.s)
 
-    def __neg__(self, /) -> A:
+    def __neg__(self, /) -> t.Self:
         n = self.data
-        n = self._mask_array_pad - n  # assumes there is a padding?
-        # n = self._mask_val + self._mask_array_val - n # works in the same way
-        return A(n, *self._shape)
+        n = self.s.mask_array_pad - n  # assumes there is a padding?
+        # n = self.s.mask_val + self.s.mask_array_val - n # works in the same way
+        return self.__class__(n, self.s)
 
-    def is_true(self, /) -> A:
+    def is_true(self, /) -> t.Self:
         n = self.data
         # store 1s in each padding block:
-        n |= self._mask_array_pad
+        n |= self.s.mask_array_pad
         # subtract 1:
-        n -= self._mask_array_val
+        n -= self.s.mask_array_val
         # isolate the first padding bit: (it will underflow to 0, iff the starting value was 0)
-        n >>= self.bits_val
-        n &= self._mask_array_val
-        return A(n, *self._shape)
+        n >>= self.s.bv
+        n &= self.s.mask_array_val
+        return self.__class__(n, self.s)
 
-    def is_false(self, /) -> A:
+    def is_false(self, /) -> t.Self:
         n = self.is_true().data
-        n ^= self._mask_array_val
-        return A(n, *self._shape)
+        n ^= self.s.mask_array_val
+        return self.__class__(n, self.s)
 
-    def eq(self, other: A | int, /) -> A:
-        if isinstance(other, int):
-            other = self._from_const(other)
+    def eq(self, other: A | int, /) -> t.Self:
+        other = self._get_binop_operand(other)
         return (self ^ other).is_false()
 
-    def ne(self, other: A | int, /) -> A:
-        if isinstance(other, int):
-            other = self._from_const(other)
+    def ne(self, other: A | int, /) -> t.Self:
+        other = self._get_binop_operand(other)
         return (self ^ other).is_true()
 
-    def lt(self, other: A | int, /) -> A:
-        if isinstance(other, int):
-            other = self._from_const(other)
+    def lt(self, other: A | int, /) -> t.Self:
+        other = self._get_binop_operand(other)
         # (self-other)<0
         n = (self - other).data
         # move sign bit to the beginning
-        n >>= self.bits_val - 1
-        n &= self._mask_array_val
-        return A(n, *self._shape)
+        n >>= self.s.bv - 1
+        n &= self.s.mask_array_val
+        return self.__class__(n, self.s)
 
-    def gt(self, other: A | int, /) -> A:
-        if isinstance(other, int):
-            other = self._from_const(other)
-        # it is basically (other-self)<0
+    def gt(self, other: A | int, /) -> t.Self:
+        other = self._get_binop_operand(other)
+        # (other-self)<0
         n = (other - self).data
         # move sign bit to the beginning
-        n >>= self.bits_val - 1
-        n &= self._mask_array_val
-        return A(n, *self._shape)
+        n >>= self.s.bv - 1
+        n &= self.s.mask_array_val
+        return self.__class__(n, self.s)
 
-    def le(self, other: A | int, /) -> A:
-        if isinstance(other, int):
-            other = self._from_const(other)
+    def le(self, other: A | int, /) -> t.Self:
         return self.lt(other) | self.eq(other)
 
-    def ge(self, other: A | int, /) -> A:
-        if isinstance(other, int):
-            other = self._from_const(other)
+    def ge(self, other: A | int, /) -> t.Self:
         return self.gt(other) | self.eq(other)
